12d6c9689378a9de74762d585ad35df414dcfd47
Joseph P.Vera <148766987+JosephPVera@users.noreply.github.com>
2024-10-28 23:34:39 +0100

    Update localized_beta.py

--- DFT/scripts/in-progress/localized_beta.py
+++ DFT/scripts/in-progress/localized_beta.py
@@ -4,6 +4,7 @@
 
 "This code need to implement new ideas, but can plot band vs tot for check the localized states."
 "IMPROVE: VBM, CBM and 1000 (in for j in range(max(0, i - 1000)). branches??? " 
+"sum the first 5 values ​​closest to 1"
 import xml.etree.ElementTree as ET
 import os
 import argparse
