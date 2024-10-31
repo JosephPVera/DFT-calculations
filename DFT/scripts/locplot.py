9d434207a527aae1fbbd824847cbd04ff9a88493
Joseph P.Vera <148766987+JosephPVera@users.noreply.github.com>
2024-10-31 15:00:36 +0100

    Update locplot.py

--- DFT/scripts/locplot.py
+++ DFT/scripts/locplot.py
@@ -6,7 +6,6 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.ndimage import gaussian_filter1d
 import os
 from io import StringIO
 
